efc33335483d691030a257dbd2b4382f2fa13ff9
Admin <Admin@AF-PC>
2018-01-24 21:49:42 +0600

    Add project files.

---
+++ HW18012018/Model.cs
@@ -0,0 +1,25 @@
+namespace WH18012018
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model : DbContext
+    {
+        public Model()
+            : base("name=DateBase")
+        {
+        }
+
+        public virtual DbSet<Area> Area { get; set; }
+        public virtual DbSet<Timer> Timer { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Area>()
+                .Property(e => e.IP)
+                .IsUnicode(false);
+        }
+    }
+}
